80655c0f1deace5e3b5f6811afaa61ccf84012b0
Kagenooi <kagenooi.morpheus@gmail.com>
2024-12-21 19:56:54 +0400

    asd

--- js/favorites.js
+++ js/favorites.js
@@ -3,29 +3,21 @@ window.adaptive = adaptive;
 window.addEventListener('resize', function () { adaptive() });
 adaptive();
 const navbarMobile = document.querySelector('.navbar__mobile_btns_menu');
-navbarMobile?.addEventListener('click', function () {
+navbarMobile?.addEventListener('click', function() {
     document.querySelector('.navbar__mobile_menu').classList.toggle('active');
 })
 const navbarMobileClose = document.querySelector('.navbar__mobile_menu_header_closeBtn');
-navbarMobileClose?.addEventListener('click', function () {
+navbarMobileClose?.addEventListener('click', function() {
     document.querySelector('.navbar__mobile_menu').classList.toggle('active');
 })
 
 
-const swiper = new Swiper(".mySwiper", {
-    spaceBetween: 10,
-    slidesPerView: 4,
-    freeMode: true,
-    watchSlidesProgress: true,
-});
-const swiper2 = new Swiper(".mySwiper2", {
-    spaceBetween: 10,
+let buba = new Swiper('.catalog__el_slider', {
+    slidesPerView: 1,
     loop: true,
     navigation: {
-        nextEl: ".productWrapper__gallery_btns_btn.next",
-        prevEl: ".productWrapper__gallery_btns_btn.prev",
-    },
-    thumbs: {
-        swiper: swiper,
-    },
+        nextEl: `.catalog__el_slider .catalog__el_slider_btns_btn.next`,
+        prevEl: `.catalog__el_slider .catalog__el_slider_btns_btn.prev`,
+    }
 });
+window.buba = buba;
